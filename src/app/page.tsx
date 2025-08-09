d56ef2b9d015cd00a3c7aad7862962352af2b18f
rob <heidlessemail14@gmail.com>
2025-08-09 21:30:50 +0000

    header appearing twice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
 import Hero from "@/components/sections/Hero";
 import About from "@/components/sections/About";
 import Services from "@/components/sections/Services";
@@ -10,18 +8,14 @@ import Contact from "@/components/sections/Contact";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      <main className="flex-1">
-        <Hero />
-        <About />
-        <Services />
-        <Cta />
-        <AiProTips />
-        <Testimonials />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Hero />
+      <About />
+      <Services />
+      <Cta />
+      <AiProTips />
+      <Testimonials />
+      <Contact />
+    </>
   );
 }
